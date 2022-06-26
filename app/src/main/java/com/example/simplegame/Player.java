562151f79c5cd9c8821e5e3ce6cd6a88369fd9ad
Israel <israel@Softbinder.local>
2022-06-26 16:51:43 +0300

    Game loop

---
+++ app/src/main/java/com/example/simplegame/Player.java
@@ -0,0 +1,24 @@
+package com.example.simplegame;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Player {
+
+    private double positionX;
+    private double positionY;
+    private double radius;
+    private Paint paint;
+
+    public Player(double positionX, double positionY){
+
+    }
+
+    public void draw(Canvas canvas){
+
+    }
+
+    public void update() {
+
+    }
+}
